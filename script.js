7b0c9db5476eecc2f1c3b28d2c94acdaa2296186
Denis Lisunov <rev.denisas@gmail.com>
2021-07-01 15:04:33 +0300

    added rock paper scrissors script code

---
+++ script.js
@@ -0,0 +1,42 @@
+const ROCK = "rock";
+const PAPER = "paper";
+const SCRISSORS = "scissors"
+const options = [ROCK, PAPER, SCRISSORS];
+const rockWin = "You win! Rock breaks scissors.";
+const rockLose = "You lose! Paper covers rock."
+const paperWin = "You win! Paper covers rock.";
+const paperLose = "You lose! Scissors cut paper."
+const sciWin = "You win! Scissors cut paper.";
+const sciLose = "You lose! Rock breaks scissors."
+
+function computerPlay(){
+    return options[Math.floor(Math.random() * 3)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection){
+        return console.log(`IT'S A TIE!`);
+
+    } else if (playerSelection == ROCK && computerSelection == PAPER){
+        return console.log(rockLose);
+
+    } else if (playerSelection == ROCK && computerSelection == SCRISSORS){
+        return console.log(rockWin);
+
+    } else if (playerSelection == PAPER && computerSelection == ROCK){
+        return console.log(paperWin);
+
+    } else if (playerSelection == PAPER && computerSelection == SCRISSORS){
+        return console.log(paperLose);
+
+    } else if (playerSelection == SCRISSORS && computerSelection == ROCK){
+        return console.log(sciLose);
+    } else {
+        return console.log(sciWin);
+    }
+}
+  const playerSelection = options[0];
+  const computerSelection = computerPlay();
+  console.log(playerSelection);
+  console.log(computerSelection);
+  playRound(playerSelection, computerSelection);
